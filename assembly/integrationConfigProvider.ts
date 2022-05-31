6eb9f8a54a367592a8440266eec5098ce4bf2e4a
Vasil <vava@queue-it.com>
2022-05-31 15:52:26 +0200

    Worker host can now be configured.

--- assembly/integrationConfigProvider.ts
+++ assembly/integrationConfigProvider.ts
@@ -1,110 +1,146 @@
-import {Fastly, Headers, Request} from "@fastly/as-compute";
-import {RequestLogger} from "./helper";
+import { Fastly, Headers, Request } from "@fastly/as-compute";
+import { RequestLogger } from "./helper";
 
-export function getIntegrationConfig(details: IntegrationDetails, endpointProvider: IntegrationEndpointProvider): string {
-    const headers = new Headers();
-    headers.set('api-key', details.apiKey);
-    headers.set('host', endpointProvider.getHostname(details.customerId));
-    const request = new Request(endpointProvider.getEndpoint(details.customerId), {
-        method: 'GET', body: null, headers: headers
-    })
-    let cacheOverride = new Fastly.CacheOverride();
-    let cacheConf = endpointProvider.getCacheConfig();
-    if (cacheConf.maxAge != -1) {
-        cacheOverride.setTTL(cacheConf.maxAge);
+export function getIntegrationConfig(
+  details: IntegrationDetails,
+  endpointProvider: IntegrationEndpointProvider
+): string {
+  const headers = new Headers();
+  headers.set("api-key", details.apiKey);
+  headers.set("host", endpointProvider.getHostname(details.customerId));
+  const request = new Request(
+    endpointProvider.getEndpoint(details.customerId),
+    {
+      method: "GET",
+      body: null,
+      headers: headers,
     }
-    if (cacheConf.staleWhileRevalidate != -1) {
-        cacheOverride.setSWR(cacheConf.staleWhileRevalidate);
-    }
+  );
+  let cacheOverride = new Fastly.CacheOverride();
+  let cacheConf = endpointProvider.getCacheConfig();
+  if (cacheConf.maxAge != -1) {
+    cacheOverride.setTTL(cacheConf.maxAge);
+  }
+  if (cacheConf.staleWhileRevalidate != -1) {
+    cacheOverride.setSWR(cacheConf.staleWhileRevalidate);
+  }
 
-    let beresp = Fastly.fetch(request, {
-        backend: details.queueItOrigin,
-        cacheOverride: cacheOverride
-    }).wait();
+  let beresp = Fastly.fetch(request, {
+    backend: details.queueItOrigin,
+    cacheOverride: cacheOverride,
+  }).wait();
 
-    if (!(details.logger instanceof MockLogger)) {
-        let cacheState = beresp.headers.get('x-cache');
-        let hits = beresp.headers.get('x-cache-hits');
-        if (hits == null) hits = '-1';
-        if (cacheState == null) cacheState = 'n';
-        details.logger.log("IgnFetch:" + cacheState! + ":" + hits!);
-    }
+  if (!(details.logger instanceof MockLogger)) {
+    let cacheState = beresp.headers.get("x-cache");
+    let hits = beresp.headers.get("x-cache-hits");
+    if (hits == null) hits = "-1";
+    if (cacheState == null) cacheState = "n";
+    details.logger.log("IgnFetch:" + cacheState! + ":" + hits!);
+  }
 
-    if (beresp.status != 200) {
-        return '';
-    }
-    return beresp.text();
+  if (beresp.status != 200) {
+    return "";
+  }
+  return beresp.text();
 }
 
 const integrationCustomerId = "customerId",
-    integrationApiKey = 'apiKey',
-    integrationSecret = 'secret',
-    integrationQueueItOrigin = 'queueItOrigin',
-    integrationDictionary = "IntegrationConfiguration";
+  integrationApiKey = "apiKey",
+  integrationSecret = "secret",
+  integrationQueueItOrigin = "queueItOrigin",
+  integrationDictionary = "IntegrationConfiguration",
+  workerHost = "workerHost";
 
 export function resolveIntegrationDetails(): IntegrationDetails | null {
-    const dict = new Fastly.Dictionary(integrationDictionary);
-    if (!dict.contains(integrationCustomerId)
-        || !dict.contains(integrationApiKey)
-        || !dict.contains(integrationSecret)
-        || !dict.contains(integrationQueueItOrigin)) {
-        return null;
-    }
+  const dict = new Fastly.Dictionary(integrationDictionary);
+  if (
+    !dict.contains(integrationCustomerId) ||
+    !dict.contains(integrationApiKey) ||
+    !dict.contains(integrationSecret) ||
+    !dict.contains(integrationQueueItOrigin)
+  ) {
+    return null;
+  }
 
-    return new IntegrationDetails(
-        dict.get(integrationQueueItOrigin)!,
-        dict.get(integrationCustomerId)!,
-        dict.get(integrationSecret)!,
-        dict.get(integrationApiKey)!)
+  let workerHostValue = "";
+  if (dict.contains(workerHost)) {
+    workerHostValue = dict.get(workerHost)!;
+  }
+
+  return new IntegrationDetails(
+    dict.get(integrationQueueItOrigin)!,
+    dict.get(integrationCustomerId)!,
+    dict.get(integrationSecret)!,
+    dict.get(integrationApiKey)!,
+    workerHostValue
+  );
 }
 
 export class IntegrationEndpointCacheConfig {
-    maxAge: i16 = -1;
-    staleWhileRevalidate: i16 = -1;
+  maxAge: i16 = -1;
+  staleWhileRevalidate: i16 = -1;
 }
 
 export interface IntegrationEndpointProvider {
-    getHostname(customerId: string): string;
+  getHostname(customerId: string): string;
 
-    getEndpoint(customerId: string): string;
+  getEndpoint(customerId: string): string;
 
-    getCacheConfig(): IntegrationEndpointCacheConfig;
+  getCacheConfig(): IntegrationEndpointCacheConfig;
 }
 
-export class QueueItIntegrationEndpointProvider implements IntegrationEndpointProvider {
-    private readonly cacheConfig: IntegrationEndpointCacheConfig;
+export class QueueItIntegrationEndpointProvider
+  implements IntegrationEndpointProvider
+{
+  private readonly cacheConfig: IntegrationEndpointCacheConfig;
 
-    constructor() {
-        this.cacheConfig = new IntegrationEndpointCacheConfig();
-        this.cacheConfig.maxAge = 60 * 5;
-        this.cacheConfig.staleWhileRevalidate = 60 * 5;
-    }
+  constructor() {
+    this.cacheConfig = new IntegrationEndpointCacheConfig();
+    this.cacheConfig.maxAge = 60 * 5;
+    this.cacheConfig.staleWhileRevalidate = 60 * 5;
+  }
 
-    getCacheConfig(): IntegrationEndpointCacheConfig {
-        return this.cacheConfig;
-    }
+  getCacheConfig(): IntegrationEndpointCacheConfig {
+    return this.cacheConfig;
+  }
 
-    getHostname(customerId: string): string {
-        return customerId + ".queue-it.net";
-    }
+  getHostname(customerId: string): string {
+    return customerId + ".queue-it.net";
+  }
 
-    getEndpoint(customerId: string): string {
-        const host = this.getHostname(customerId);
-        return "https://" + host + "/status/integrationconfig/secure/" + customerId;
-    }
+  getEndpoint(customerId: string): string {
+    const host = this.getHostname(customerId);
+    return "https://" + host + "/status/integrationconfig/secure/" + customerId;
+  }
 }
 
 class MockLogger implements RequestLogger {
-    log(message: string): void {
-    }
+  log(message: string): void {}
 }
 
 export class IntegrationDetails {
-    constructor(public queueItOrigin: string,
-                public customerId: string,
-                public secretKey: string,
-                public apiKey: string,
-                public provider: IntegrationEndpointProvider = new QueueItIntegrationEndpointProvider(),
-                public logger: RequestLogger = new MockLogger()) {
+  constructor(
+    public queueItOrigin: string,
+    public customerId: string,
+    public secretKey: string,
+    public apiKey: string,
+    public workerHost: string,
+    public provider: IntegrationEndpointProvider = new QueueItIntegrationEndpointProvider(),
+    public logger: RequestLogger = new MockLogger()
+  ) {}
+
+  resolveWorkerRequestUrl(pureUrl: string): string {
+    if (this.workerHost == "") {
+      return pureUrl;
     }
+    const protoEnding = pureUrl.indexOf("://") + 3;
+    const protocol = pureUrl.substr(0, protoEnding);
+    const urlWithoutProto = pureUrl.substr(protoEnding);
+    const pathAndQuery =
+      urlWithoutProto.indexOf("/") != -1
+        ? urlWithoutProto.substr(urlWithoutProto.indexOf("/"))
+        : "";
+    const rewrittenUrl = protocol + this.workerHost + pathAndQuery;
+    return rewrittenUrl;
+  }
 }
--- assembly/requestResponseHandler.ts
+++ assembly/requestResponseHandler.ts
@@ -1,104 +1,136 @@
-import {Request, Response, Headers} from "@fastly/as-compute";
-import {KnownUser} from "./sdk/KnownUser";
-import {QueueITHelper} from "./helper";
-import {FastlyHttpContextProvider, getHttpHandler} from "./contextProvider";
+import { Request, Response, Headers } from "@fastly/as-compute";
+import { KnownUser } from "./sdk/KnownUser";
+import { QueueITHelper } from "./helper";
+import { FastlyHttpContextProvider, getHttpHandler } from "./contextProvider";
 import {
-    getIntegrationConfig,
-    resolveIntegrationDetails,
-    IntegrationDetails,
-    QueueItIntegrationEndpointProvider
-} from "./integrationConfigProvider"
-import {Utils} from "./sdk/QueueITHelpers";
+  getIntegrationConfig,
+  resolveIntegrationDetails,
+  IntegrationDetails,
+  QueueItIntegrationEndpointProvider,
+} from "./integrationConfigProvider";
+import { Utils } from "./sdk/QueueITHelpers";
 
-const QUEUEIT_FAILED_HEADERNAME = 'x-queueit-failed';
+const QUEUEIT_FAILED_HEADERNAME = "x-queueit-failed";
 let httpProvider: FastlyHttpContextProvider | null = null;
 
-export function onQueueITRequest(req: Request, conf: IntegrationDetails | null = null): Response | null {
-    if (conf == null) {
-        conf = resolveIntegrationDetails();
+export function onQueueITRequest(
+  req: Request,
+  conf: IntegrationDetails | null = null
+): Response | null {
+  if (conf == null) {
+    conf = resolveIntegrationDetails();
+  }
+  if (conf == null) {
+    return new Response(String.UTF8.encode("No integration details found."), {
+      headers: new Headers(),
+      status: 404,
+      url: "",
+    });
+  }
+
+  const integrationProvider =
+    conf.provider == null
+      ? new QueueItIntegrationEndpointProvider()
+      : conf.provider;
+  QueueITHelper.configureKnownUserHashing();
+  httpProvider = getHttpHandler(req);
+
+  let integrationConfigJson = getIntegrationConfig(conf, integrationProvider);
+  const requestUrl: string = conf.resolveWorkerRequestUrl(req.url);
+
+  const queueItToken = Utils.getParameterByName(
+    requestUrl,
+    KnownUser.QueueITTokenKey
+  );
+  const requestUrlWithoutToken: string = Utils.removeQueueItToken(requestUrl);
+
+  // The requestUrlWithoutToken is used to match Triggers and as the Target url (where to return the users to).
+  // It is therefor important that this is exactly the url of the users browsers. So, if your webserver is
+  // behind e.g. a load balancer that modifies the host name or port, reformat requestUrlWithoutToken before proceeding.
+  const validationResultPair = KnownUser.validateRequestByIntegrationConfig(
+    requestUrlWithoutToken,
+    queueItToken,
+    integrationConfigJson,
+    conf.customerId,
+    conf.secretKey,
+    httpProvider!
+  );
+
+  if (
+    validationResultPair.first != null &&
+    validationResultPair.first!.doRedirect()
+  ) {
+    const validationResult = validationResultPair.first!;
+
+    if (validationResult.isAjaxResult) {
+      let response = new Response(null, {
+        status: 200,
+        headers: httpProvider!.getHttpResponse().getHeaders(),
+        url: "",
+      });
+      // In case of ajax call send the user to the queue by sending a custom queue-it header and redirecting user to queue from javascript
+      response.headers.set(
+        validationResult.getAjaxQueueRedirectHeaderKey(),
+        QueueITHelper.addKUPlatformVersion(
+          validationResult.getAjaxRedirectUrl()
+        )
+      );
+      Utils.addNoCacheHeaders(response);
+      return response;
+    } else {
+      let response = new Response(null, {
+        status: 302,
+        headers: httpProvider!.getHttpResponse().getHeaders(),
+        url: "",
+      });
+      // Send the user to the queue - either because hash was missing or because is was invalid
+      response.headers.set(
+        "Location",
+        QueueITHelper.addKUPlatformVersion(validationResult.redirectUrl)
+      );
+      Utils.addNoCacheHeaders(response);
+      return response;
     }
-    if (conf == null) {
-        return new Response(String.UTF8.encode("No integration details found."), {
-            headers: new Headers(),
-            status: 404,
-            url: ''
-        });
+  } else if (validationResultPair.first != null) {
+    const validationResult = validationResultPair.first!;
+    // Request can continue - we remove queueittoken form querystring parameter to avoid sharing of user specific token
+    // Support mobile scenario adding the condition !validationResult.isAjaxResult
+    if (
+      queueItToken != "" &&
+      !validationResult.isAjaxResult &&
+      validationResult.actionType == "Queue"
+    ) {
+      let response = new Response(null, {
+        status: 302,
+        headers: httpProvider!.getHttpResponse().getHeaders(),
+        url: requestUrlWithoutToken,
+      });
+      response.headers.set("Location", requestUrlWithoutToken);
+      Utils.addNoCacheHeaders(response);
+      return response;
+    } else {
+      // lets caller decide the next step, or just serve the request normally
+      return null;
     }
+  } else if (validationResultPair.second != null) {
+    httpProvider!.isError = true;
+  }
 
-    const integrationProvider = conf.provider == null ? new QueueItIntegrationEndpointProvider() : conf.provider;
-    QueueITHelper.configureKnownUserHashing();
-    httpProvider = getHttpHandler(req);
-
-    let integrationConfigJson = getIntegrationConfig(conf, integrationProvider);
-    const requestUrl: string = req.url;
-    const queueItToken = Utils.getParameterByName(requestUrl, KnownUser.QueueITTokenKey);
-    const requestUrlWithoutToken: string = Utils.removeQueueItToken(requestUrl);
-    // The requestUrlWithoutToken is used to match Triggers and as the Target url (where to return the users to).
-    // It is therefor important that this is exactly the url of the users browsers. So, if your webserver is
-    // behind e.g. a load balancer that modifies the host name or port, reformat requestUrlWithoutToken before proceeding.
-    const validationResultPair = KnownUser.validateRequestByIntegrationConfig(requestUrlWithoutToken, queueItToken, integrationConfigJson,
-        conf.customerId, conf.secretKey, httpProvider!);
-
-    if (validationResultPair.first != null &&
-        validationResultPair.first!.doRedirect()) {
-        const validationResult = validationResultPair.first!;
-
-        if (validationResult.isAjaxResult) {
-            let response = new Response(null, {
-                status: 200,
-                headers: httpProvider!.getHttpResponse().getHeaders(),
-                url: ''
-            });
-            // In case of ajax call send the user to the queue by sending a custom queue-it header and redirecting user to queue from javascript
-            response.headers.set(validationResult.getAjaxQueueRedirectHeaderKey(), QueueITHelper.addKUPlatformVersion(validationResult.getAjaxRedirectUrl()));
-            Utils.addNoCacheHeaders(response);
-            return response;
-        } else {
-            let response = new Response(null, {
-                status: 302,
-                headers: httpProvider!.getHttpResponse().getHeaders(),
-                url: ''
-            });
-            // Send the user to the queue - either because hash was missing or because is was invalid
-            response.headers.set('Location', QueueITHelper.addKUPlatformVersion(validationResult.redirectUrl));
-            Utils.addNoCacheHeaders(response);
-            return response;
-        }
-    } else if (validationResultPair.first != null) {
-        const validationResult = validationResultPair.first!;
-        // Request can continue - we remove queueittoken form querystring parameter to avoid sharing of user specific token
-        // Support mobile scenario adding the condition !validationResult.isAjaxResult
-        if (queueItToken != '' && !validationResult.isAjaxResult && validationResult.actionType == 'Queue') {
-            let response = new Response(null, {
-                status: 302,
-                headers: httpProvider!.getHttpResponse().getHeaders(),
-                url: requestUrlWithoutToken
-            });
-            response.headers.set('Location', requestUrlWithoutToken);
-            Utils.addNoCacheHeaders(response);
-            return response;
-        } else {
-            // lets caller decide the next step, or just serve the request normally
-            return null;
-        }
-    } else if (validationResultPair.second != null) {
-        httpProvider!.isError = true;
-    }
-
-    return null;
+  return null;
 }
 
 //Fill in the Queue-it headers
 export function onQueueITResponse(res: Response): void {
-    const contextHeaders = httpProvider!.getHttpResponse().getHeaders();
-    const contextHeaderKeys = contextHeaders.keys();
+  const contextHeaders = httpProvider!.getHttpResponse().getHeaders();
+  const contextHeaderKeys = contextHeaders.keys();
 
-    if (httpProvider!.isError) {
-        res.headers.append(QUEUEIT_FAILED_HEADERNAME, "true");
-    }
-    for (let i = 0; i < contextHeaderKeys.length; i++) {
-        if (contextHeaderKeys[i].length == 0) continue;
-        let value = contextHeaders.get(contextHeaderKeys[i]);
-        if (value != null && value!.length > 0) res.headers.set(contextHeaderKeys[i], value!);
-    }
+  if (httpProvider!.isError) {
+    res.headers.append(QUEUEIT_FAILED_HEADERNAME, "true");
+  }
+  for (let i = 0; i < contextHeaderKeys.length; i++) {
+    if (contextHeaderKeys[i].length == 0) continue;
+    let value = contextHeaders.get(contextHeaderKeys[i]);
+    if (value != null && value!.length > 0)
+      res.headers.set(contextHeaderKeys[i], value!);
+  }
 }
